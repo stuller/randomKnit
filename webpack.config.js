9759201c14d57a001ae10ca4d59b7dad8796681c
Summer Tuller <stuller@digitalriver.com>
2022-02-16 15:00:57 -0600

    update output to docs

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   mode: 'development',
   entry: path.join(__dirname, "src", "app.js"),
   output: {
-    path:path.resolve(__dirname, "dist"),
+    path:path.resolve(__dirname, "docs"),
     filename: 'bundle.js',
   },
   module: {
